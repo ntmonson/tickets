27e7711055f0d0b84967fa33ffaa272096b56dfc
Nick Monson <ntmonson@gmail.com>
2018-02-11 15:50:54 -0700

    Changed addtickets/index.js to a regular function so that webpack could build it, it wasn't taking advantage of async anyway.

--- src/routes/addtickets/index.js
+++ src/routes/addtickets/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AddTickets from './AddTickets';
 import Layout from '../../components/Layout';
 
-async function action() {
+function action() {
   return {
     title: 'Nick Monson Ticket App',
     component: (
